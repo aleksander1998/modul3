ce43530af4f4783d8232b5c67f50da2abbe6b5a2
aleksander1998 <57661998+aleksander1998@users.noreply.github.com>
2020-06-19 07:47:55 +0200

    d

---
+++ ConsoleApp1/ConsoleApp1/Piece.cs
@@ -0,0 +1,25 @@
+﻿using  System;
+
+namespace SimpleChess
+{
+    
+        public abstract class Piece
+        {
+            public string Symbol { get; set; }
+
+
+            protected Piece(string symbol)
+            {
+
+                Symbol = symbol;
+            }
+
+            protected Piece()
+            {
+            }
+
+            public abstract bool Move(string fromPosition, string toPosition);
+        }
+    }
+
+
